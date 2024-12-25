dc26b3a9b93c9ba627e8d7ae94e5b022be0690e8
ByteAtATime <byteatatime@proton.me>
2024-12-25 11:30:42 -0800

    feat: add clerk for authentication

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import ClerkProvider from 'clerk-sveltekit/client/ClerkProvider.svelte';
+	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+	{@render children()}
+</ClerkProvider>
