82e6799437909cdbfb1c74a0f0afcf57c43cae22
ByteAtATime <byteatatime@proton.me>
2024-12-25 12:33:02 -0800

    refactor: remove redundant constructor

--- src/lib/server/devices/postgres.ts
+++ src/lib/server/devices/postgres.ts
@@ -4,8 +4,6 @@ import { devicesTable } from '../db/schema';
 import { db } from '../db';
 
 export class PostgresDeviceRepository implements IDeviceRepository {
-	constructor() {}
-
 	async getDeviceById(id: number): Promise<Device | null> {
 		const device = await db.query.devicesTable.findFirst({
 			where: eq(devicesTable.id, id)
