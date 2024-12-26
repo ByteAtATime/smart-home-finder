a4ce658148c89b171e69053d13b6f0ef38cc7a28
ByteAtATime <byteatatime@proton.me>
2024-12-25 17:42:45 -0800

    feat: shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
 	import '../app.css';
 	import ClerkProvider from 'clerk-sveltekit/client/ClerkProvider.svelte';
 	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
 <ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
-	{@render children()}
+	<ModeWatcher />
+	{@render children?.()}
 </ClerkProvider>
