e97b1b89dd7244170b42494e4e387ad98b1b65b2
ByteAtATime <byteatatime@proton.me>
2024-12-25 20:52:23 -0800

    feat: add db seeding script

---
+++ src/lib/server/db/seed.ts
@@ -0,0 +1,41 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from './schema';
+import { reset } from 'drizzle-seed';
+
+async function main() {
+	const db = drizzle(process.env.DATABASE_URL!);
+
+	console.log('Resetting database');
+	await reset(db, schema);
+
+	console.log('Inserting device');
+	const [device] = await db
+		.insert(schema.devicesTable)
+		.values({
+			name: 'Inovelli Smart Dimmer (Blue)',
+			deviceType: 'switch',
+			protocol: 'zigbee'
+		})
+		.returning();
+
+	console.log('Inserting property');
+	const [property] = await db
+		.insert(schema.propertiesTable)
+		.values({
+			name: 'Voltage',
+			type: 'float',
+			unit: 'V'
+		})
+		.returning();
+
+	console.log('Inserting device property');
+	await db.insert(schema.devicePropertiesTable).values({
+		deviceId: device.id,
+		propertyId: property.id,
+		floatValue: 120
+	});
+
+	console.log('Done');
+}
+
+main();
